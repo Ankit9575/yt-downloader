aa4841ffdcba7bc4fd73ae4f46c580d1ec54a35a
Ankit kushwah <kushwahankit9575@gmail.com>
2025-04-28 21:19:45 +0530

    Updated app.py and added cookies.txt

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ def index():
 def download_video():
     url = request.form['url']
     quality = request.form['quality']
-
+    
+    # Path to your cookies.txt file
+    cookie_path = r'C:\Users\as\vs code\youtube_downloader\cookies.txt'  # Update this path as needed
+    
     # First, get the video info without downloading
-    with yt_dlp.YoutubeDL({}) as ydl:
+    with yt_dlp.YoutubeDL({'cookiefile': cookie_path}) as ydl:  # Adding cookiefile to yt-dlp options
         info_dict = ydl.extract_info(url, download=False)
         video_title = info_dict.get('title', None)
     
@@ -22,8 +25,9 @@ def download_video():
     safe_title = "".join(x for x in video_title if x.isalnum() or x in " _-")
     filename = f"{safe_title}.mp4"
 
-    # Set yt-dlp options
+    # Set yt-dlp options with cookies and quality filter
     ydl_opts = {
+        'cookiefile': cookie_path,  # Ensure the cookies are used here as well
         'format': f'bestvideo[height<={quality}]+bestaudio/best[height<={quality}]',
         'outtmpl': filename,
         'merge_output_format': 'mp4',
@@ -33,6 +37,7 @@ def download_video():
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
+    # Remove the file after sending it to the user
     @after_this_request
     def remove_file(response):
         try:
@@ -41,6 +46,7 @@ def download_video():
             print(f"Error deleting file: {e}")
         return response
 
+    # Send the downloaded file to the user
     return send_file(filename, as_attachment=True, download_name=filename)
 
 if __name__ == '__main__':
